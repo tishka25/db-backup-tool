39904996e9b4df130b8a03013e8809ad4a682a52
tishka25 <teodorstanishev@gmail.com>
2021-12-19 17:22:37 +0200

    add implementaion for reading from config.json & watching for file changes

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-import { createBackup, deleteBackupOlderThat, startBackupJob } from './backup.js';
+import { watchFile } from 'fs';
+import * as fs from 'fs/promises';
+import path from 'path';
+import { startBackupJob } from './backup.js';
 
-const config = { interval: "* * * * * *", historyTimeout: 1, outputDirectory: 'output-files' };
-const dbConfig = { address: 'localhost', db: 'testdb', user: 'teodorstanishev' };
-startBackupJob(config, dbConfig);
+async function readConfiguration() {
+  try {
+    const response = await fs.readFile(path.normalize('./config.json'));
+    return JSON.parse(response.toString());
+  } catch (error) {
+    console.error(error);
+    console.info("No configuration supplied");
+    return false;
+  }
+}
+
+async function startJob() {
+  const configuration = await readConfiguration();
+  if(configuration){
+    const config = {
+      interval: configuration.interval,
+      outputDirectory: configuration.outputDirectory,
+      historyTimeout: configuration.historyTimeout
+    };
+    startBackupJob(config, configuration.dbConfig);
+  }
+}
+
+async function main() {
+  startJob();
+  const onChange = () => {
+    console.log('config.json has been changed. Restarting...');
+    // Start job again
+    startJob();
+  }
+  const watchConfig = { persistent: true, interval: 1000 };
+  watchFile(path.normalize('./config.json'), watchConfig, onChange);
+}
+
+main();
